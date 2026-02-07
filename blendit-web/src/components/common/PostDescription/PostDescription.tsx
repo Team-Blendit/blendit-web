5d874ef93ae03ec6122d8b91dd83c9c6b93ef49c
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-02-07 16:38:57 +0900

    chore: 본문 줄간격 조정

--- blendit-web/src/components/common/PostDescription/PostDescription.tsx
+++ blendit-web/src/components/common/PostDescription/PostDescription.tsx
@@ -22,7 +22,7 @@ export const PostDescription: React.FC<PostDescriptionProps> = ({
       <div className="bg-[var(--bg-section)] flex items-center justify-center p-[30px] rounded-[18px] w-full">
         {isHtml ? (
           <div
-            className="flex-1 font-normal text-[18px] leading-[24px] text-[var(--text-primary)] [&_h1]:text-[32px] [&_h1]:font-bold [&_h1]:leading-[40px] [&_h1]:my-4 [&_h2]:text-[24px] [&_h2]:font-semibold [&_h2]:leading-[32px] [&_h2]:my-3 [&_h3]:text-[20px] [&_h3]:font-semibold [&_h3]:leading-[28px] [&_h3]:my-2 [&_p]:my-2 [&_ul]:list-disc [&_ul]:pl-6 [&_ul]:my-2 [&_ol]:list-decimal [&_ol]:pl-6 [&_ol]:my-2 [&_li]:my-1 [&_strong]:font-bold [&_em]:italic [&_u]:underline [&_a]:text-blue-500 [&_a]:underline"
+            className="flex-1 font-normal text-[18px] leading-[28px] text-[var(--text-primary)] [&_h1]:text-[32px] [&_h1]:font-bold [&_h1]:leading-[44px] [&_h1]:my-0 [&_h2]:text-[24px] [&_h2]:font-semibold [&_h2]:leading-[36px] [&_h2]:my-0 [&_h3]:text-[20px] [&_h3]:font-semibold [&_h3]:leading-[32px] [&_h3]:my-0 [&_p]:my-0 [&_ul]:list-disc [&_ul]:pl-6 [&_ul]:my-0 [&_ol]:list-decimal [&_ol]:pl-6 [&_ol]:my-0 [&_li]:my-0 [&_strong]:font-bold [&_em]:italic [&_u]:underline [&_a]:text-blue-500 [&_a]:underline"
             dangerouslySetInnerHTML={{ __html: content }}
           />
         ) : (
