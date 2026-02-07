76c87be7f23dfacfb63874949605b00eb7e8a3e4
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-02-08 03:13:18 +0900

    chore: metadata 수정

--- blendit-web/src/app/layout.tsx
+++ blendit-web/src/app/layout.tsx
@@ -3,8 +3,22 @@ import "./globals.css";
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://blendit.kr'),
   title: "BlendIt",
   description: "IT 직군을 위한 네트워킹 플랫폼 BlendIt",
+  openGraph: {
+    title: "BlendIt",
+    description: "IT 직군을 위한 네트워킹 플랫폼 BlendIt",
+    images: ["images/thumbnail.png"],
+    type: "website",
+    url: "https://blendit.kr",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "BlendIt",
+    description: "IT 직군을 위한 네트워킹 플랫폼 BlendIt",
+    images: ["images/thumbnail.png"],
+  },
 };
 
 export default function RootLayout({
